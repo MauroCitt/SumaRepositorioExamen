22d6ad7396fab1c0076e669e4924f71f8633532b
MauroCitt <maci290@vidalibarraquer.net>
2023-05-12 10:29:33 +0200

    Método ValorAbsoluto

---
+++ src/suma/Metodos.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package suma;
+
+/**
+ *
+ * @author alumne
+ */
+public class Metodos {
+
+    public int valorAbsoluto(int a, int b) {
+        int res = Math.abs(a) + Math.abs(b);
+        return res;
+    }
+
+}
