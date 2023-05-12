5edf52fee1055cb339ae3ba284fdb6b05e757603
MauroCitt <maci290@vidalibarraquer.net>
2023-05-12 10:12:25 +0200

    commit 1

--- src/suma/Suma.java
+++ src/suma/Suma.java
@@ -14,7 +14,9 @@ public class Suma {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        //*Proyecto de M05
+        
     }
     
 }
