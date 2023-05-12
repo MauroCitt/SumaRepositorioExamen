ad9352bf8e38113c778fa332567646c2a22d7cf1
MauroCitt <maci290@vidalibarraquer.net>
2023-05-12 10:25:23 +0200

    Metodo GetNum1

---
+++ src/suma/Metodos.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package suma;
+
+/**
+ *
+ * @author alumne
+ */
+public class Metodos {
+
+    public int Sumar(int a, int b) {
+        return a;
+    }
+
+}
